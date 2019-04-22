62f3cb026ffd9fbc4a393467127baec850cb53fd
macabrus <34215978+macabrus@users.noreply.github.com>
2019-04-22 20:56:54 +0200

    Update Crtac.java

--- zadaci/src/zadaci/Crtac.java
+++ zadaci/src/zadaci/Crtac.java
@@ -2,33 +2,26 @@ package zadaci;
 import java.util.ArrayList;
 
 public class ConsoleWindowManager {
-	
-	ArrayList <ArrayList<Character>> screen=new ArrayList<>();//screen=hashtagirano polje unutar kojeg se sve odvija
-	//kaj ovo radi???
-	//centrirano? kaj je to
-	ArrayList<ArrayList<Character>> centrirano=new ArrayList<>();
+
+	char[][] screen;//screen=hashtagirano polje unutar kojeg se sve odvija
+	char[][] centrirano;
 	int screenHeight;
 	int screenWidth;
 
 	public void print() {
-		for(int i=0;i<screen.size();i++) {
-			for(int j=0;j<screen.get(i).size();j++) {
-				System.out.print(screen.get(i).get(j));
+		for(int i=0;i<screenHeight;i++) {
+			for(int j=0;j<screenWidth;j++) {
+				System.out.print(screen[i][j]);
 			}
 			System.out.println();
 		}
 	}
 
 	public void initScreen(int height, int width) {
-		screen.clear();
+		//screen.clear();
 		screenHeight =height;//kasnije mi treba kod provjeravanja jel moguce napravit zadani okvir unutar hashatagiranog polja
 		screenWidth =width;
-		for(int i=0;i<height;i++) {
-			screen.add(new ArrayList<Character>());
-			for(int j=0;j<width;j++) {
-				screen.get(i).add('.');
-			}
-		}
+		screen = new char[height][width];
 	}
 
 	public void drawWindow(int x, int y, int height, int width) {
@@ -41,15 +34,15 @@ public class ConsoleWindowManager {
 			for (int i = screenHeight -y-1; i< screenHeight -y-1+height; i++) {
 				for(int j=x;j<width+x;j++) {
 					if(i== screenHeight -y-1 || i== screenHeight -y-2+height) {
-						screen.get(i).set(j,'+');
+						screen[i][j] = '+';
 					}
 					else if(i!= screenHeight -y-1 || i!= screenHeight -y-2+height) {
 						if(j==x || j==width+x-1) {
-							screen.get(i).set(j, '+');
+							screen[i][j] = '+';
 						}
-						else {
-							screen.get(i).set(j, '.');
-						}
+						//else {
+							//screen.get(i).set(j, '.');
+						//}
 					}
 				}
 			}
@@ -58,18 +51,18 @@ public class ConsoleWindowManager {
 
 	public void drawText(int x, int y, int rows, int columns, String tekst) {
 		String [] array=tekst.trim().split("\n");
+		centrirano = new char[rows][columns];
 		for(int i = 0; i < rows; i++) {
-			centrirano.add(new ArrayList<Character>());
 			for(int j = 0; j < columns; j++) {
 				if(i >= (rows-array.length)/2 &&
 						i < rows-(rows-array.length)/2 &&
 						i-(rows-array.length)/2 < array.length &&
 						j >= (columns-array[i-(rows-array.length)/2].length())/2 &&
 						j < (columns-array[i-(rows-array.length)/2].length())/2 + array[i-(rows-array.length)/2].length()){
-					centrirano.get(i).add(array[i-(rows-array.length)/2].charAt(j-(columns-array[i-(rows-array.length)/2].length())/2));
+					centrirano[i][j] = array[i-(rows-array.length)/2].charAt(j-(columns-array[i-(rows-array.length)/2].length())/2);
 				}
 				else {
-					centrirano.get(i).add('.');
+					centrirano[i][j] = '.';
 				}
 			}
 		}
