d959cb6d43342e45ffd72b466a5ccda3f9e9f38e
leticija <leticija@y700>
2019-04-23 18:31:53 +0200

    shit

--- zadaci/src/zadaci/ConsoleWindowManager.java
+++ zadaci/src/zadaci/ConsoleWindowManager.java
@@ -1,20 +1,20 @@
 package zadaci;
 import java.util.Arrays;
 
-public class ConsoleWindowManager {
+class ConsoleWindowManager {
 
-	char[][] screen;//screen=hashtagirano polje unutar kojeg se sve odvija
+	char[][] screen;
 	char[][] centrirano;
 	int screenHeight;
 	int screenWidth;
 	
 	public ConsoleWindowManager(int width, int height){
-		initScreen(height,width);
+		initScreen(height,width);//automatski zove initScreen, ne mora ga zvati objekt
 	}
 	
 	public void print() {
-		for(int i=0;i<screenHeight;i++) {
-			for(int j=0;j<screenWidth;j++) {
+		for(int i=0;i<screen.length;i++) {
+			for(int j=0;j<screen[i].length;j++) {
 				System.out.print(screen[i][j]);
 			}
 			System.out.println();
@@ -22,7 +22,6 @@ public class ConsoleWindowManager {
 	}
 
 	public void initScreen(int height, int width) {
-		//screen.clear();
 		screenHeight =height;//kasnije mi treba kod provjeravanja jel moguce napravit zadani okvir unutar hashatagiranog polja
 		screenWidth =width;
 		screen = new char[height][width];
@@ -31,25 +30,27 @@ public class ConsoleWindowManager {
 		}
 	}
 
-	public void drawWindow(int x, int y, int height, int width) {
+	public void drawWindow(int x, int y, int height, int width,String tekst) {
 		System.out.println("height okvira:"+height+" width okvira:"+width);
 
 		if ((x+width)> screenWidth || x>= screenWidth || y>= screenHeight || y+1<height){
 			System.out.print("Okvir nije unutar polja! Nem ni ispisival\n");
 		}
 		else {
-			for (int i = screenHeight -y-1; i< screenHeight -y-1+height; i++) {
+			for (int i = screenHeight-y-1; i< screenHeight-y-1+height; i++) {
 				for(int j=x;j<width+x;j++) {
 					if(i== screenHeight -y-1 || i== screenHeight -y-2+height) {
-						screen[i][j] = '+';
+						if(j==x || j==width+x-1) {
+							screen[i][j]='*';
+						}
+						else {
+							screen[i][j]='\u2500';
+						}
 					}
 					else if(i!= screenHeight -y-1 || i!= screenHeight -y-2+height) {
 						if(j==x || j==width+x-1) {
-							screen[i][j] = '+';
+							screen[i][j] = '\u2502';
 						}
-						//else {
-							//screen.get(i).set(j, '.');
-						//}
 					}
 				}
 			}
@@ -58,6 +59,7 @@ public class ConsoleWindowManager {
 
 	public void drawText(int x, int y, int rows, int columns, String tekst) {
 		String [] array=tekst.trim().split("\n");
+		System.out.print(Arrays.toString(array)+"\n");
 		centrirano = new char[rows][columns];
 		for(int i = 0; i < rows; i++) {
 			for(int j = 0; j < columns; j++) {
@@ -66,6 +68,7 @@ public class ConsoleWindowManager {
 						i-(rows-array.length)/2 < array.length &&
 						j >= (columns-array[i-(rows-array.length)/2].length())/2 &&
 						j < (columns-array[i-(rows-array.length)/2].length())/2 + array[i-(rows-array.length)/2].length()){
+					System.out.println("i:"+i+" j:"+j);
 					centrirano[i][j] = array[i-(rows-array.length)/2].charAt(j-(columns-array[i-(rows-array.length)/2].length())/2);
 				}
 				else {
@@ -73,9 +76,5 @@ public class ConsoleWindowManager {
 				}
 			}
 		}
-	}
-
-	public void UmetniCentrirano() {
-
 	}
 }
--- zadaci/src/zadaci/Main.java
+++ zadaci/src/zadaci/Main.java
@@ -1,53 +1,14 @@
 package zadaci;
-import zadaci.Crtac;
-//import java.util.concurrent.TimeUnit;
+import zadaci.ConsoleWindowManager;
 
 public class Main {
 
 	public static void main(String[] args) {
-		ConsoleWindowManager ekran = new ConsoleWindowManager(40,60);
-		//ispravi si ostatak... nova imena metoda koja sam promenil i to...
-		objekt.Tockanje(8, 20);//visina,sirina - stvaranje polja (tockanje)
-		
-		objekt.Okviranje(0,2,3,5);//x i y su koordinate gornjeg ljevog kuta okvira (0,0 bi bil donji ljevi kut polja)
-		objekt.Ispis(objekt.screen);
-		/*
-		Stopanje(2000);
-		for(int i=0;i<56;i++) {
-			objekt.Hashtagiranje(19, 60);
-			objekt.Okviranje(i,2,3,5);
-			Stopanje(100);
-		}
-		int x=55;
-		int y=1;
-		for(int i=0;i<6;i++) {
-			for(int j=0;j<8;j++) {
-				if(j<4) {
-					y++;
-					objekt.Hashtagiranje(19, 60);
-					objekt.Okviranje(x,y,3,5);
-					Stopanje(100);
-				}
-				else {
-					objekt.Hashtagiranje(19, 60);
-					objekt.Okviranje(x,y,3,5);
-					Stopanje(100);
-					y--;
-				}
-				x--;
-			}
-		}*/
+		ConsoleWindowManager ekran = new ConsoleWindowManager(10,8);		
+		//ekran.drawWindow(0,2,3,5,"BER\nPIA");//x i y su koordinate gornjeg ljevog kuta okvira (0,0 bi bil donji ljevi kut polja)
+		//ekran.print(ekran.screen);
+		//ekran.drawText(0, 0, 5, 5, "PIA\nBER");
+		ekran.drawWindow(0, 3, 3, 3, "k");
+		ekran.print();
 	}
-	public static void Stopanje(int milisekundi) {
-		try        
-		{
-		    Thread.sleep(milisekundi);
-		} 
-		catch(InterruptedException ex) 
-		{
-		    Thread.currentThread().interrupt();
-		}
-	}
-
-	
 }
