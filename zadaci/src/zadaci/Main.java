9a831dde3aaa5797a4896cb03481ebc287f04a37
leticija <leticija@y700>
2019-04-23 20:34:07 +0200

    Dodano: centriranje u okviru

--- zadaci/src/zadaci/Main.java
+++ zadaci/src/zadaci/Main.java
@@ -1,14 +1,24 @@
 package zadaci;
+
 import zadaci.ConsoleWindowManager;
 
 public class Main {
 
 	public static void main(String[] args) {
-		ConsoleWindowManager ekran = new ConsoleWindowManager(10,8);		
-		//ekran.drawWindow(0,2,3,5,"BER\nPIA");//x i y su koordinate gornjeg ljevog kuta okvira (0,0 bi bil donji ljevi kut polja)
-		//ekran.print(ekran.screen);
-		//ekran.drawText(0, 0, 5, 5, "PIA\nBER");
-		ekran.drawWindow(0, 3, 3, 3, "k");
-		ekran.print();
+		ConsoleWindowManager ekran = new ConsoleWindowManager(50,8);		
+		
+		ekran.drawWindow(0, 6, 6, 9, "PIA\nBER");
+		ekran.print(ekran.screen);
+
+	}
+	public static void Stopanje(int milisekundi) {
+		try        
+		{
+		    Thread.sleep(milisekundi);
+		} 
+		catch(InterruptedException ex) 
+		{
+		    Thread.currentThread().interrupt();
+		}
 	}
 }
